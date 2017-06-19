859fd132a190ff643733b2048692c469f5a4a9d8
Jonathan Steinmann <jonathan.steinmann@gmail.com>
2017-06-19 15:15:37 -0400

    example container can now make an API call on button click using button component

--- app/components/Header/messages.js
+++ app/components/Header/messages.js
@@ -14,4 +14,8 @@ export default defineMessages({
     id: 'boilerplate.components.Header.features',
     defaultMessage: 'Features',
   },
+  features: {
+    id: 'boilerplate.components.Header.example',
+    defaultMessage: 'Example API Call',
+  },
 });
